d589b634b12784179cb48d20ecdf315c11bcbc5f
mihai <mihai.iordache82@gmail.com>
2025-07-16 20:25:13 +0300

    initiating project

---
+++ src/main/java/com/gini/AiCoreService2Application.java
@@ -0,0 +1,13 @@
+package com.gini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCoreService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCoreService2Application.class, args);
+    }
+
+}
